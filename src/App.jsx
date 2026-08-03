deaaab26402fcb2bc8d53efe082f82b1bcecdd66
ahmadrizal270195 <ahmadrizal270195@gmail.com>
2026-08-03 10:35:40 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ const ACTIVE_TAB_KEY = "erp-last-active-tab";
 
 // DAFTAR EMAIL USER YANG BOLEH AKSES DASHBOARD KEUANGAN & MODUL FINANCE
 const ADMIN_FINANCE_EMAILS = [
-  "finance@wiryatamaputera.co.id",
+  "ahmadrizal270195@gmail.com",
   "direktur@wiryatamaputera.co.id",
   "admin@wiryatamaputera.co.id"
 ];
